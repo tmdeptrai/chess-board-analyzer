89ea68a3f56bf23b891f8bd378617d1a91f3b295
tmd <minhduongqo@gmail.com>
2026-02-14 23:28:16 +0100

    project init with uv

---
+++ download_videos.sh
@@ -0,0 +1,1 @@
+gdown -c https://drive.google.com/drive/u/0/folders/1MQ2loTtn1RTHMlPMRSCHvCBE71CvzDq8
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from chess-board-analyzer!")
+
+
+if __name__ == "__main__":
+    main()
